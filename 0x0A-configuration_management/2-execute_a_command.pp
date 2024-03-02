74271aa834d51a54146cf58712050e921b6ac5af
youssef el ouidad <y.elouidad@gmail.com>
2024-03-02 19:20:06 +0100

    a

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,9 +0,0 @@
-# execute a command
-exec { 'pkill killmenow':
-  command     => '/usr/bin/pkill -f killmenow',
-  path        => '/usr/bin',
-  refreshonly => true,
-  returns     => [0, 1],
-  logoutput   => true,
-}
-
